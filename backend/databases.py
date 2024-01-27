54aa50bc5e53a11be5fcf5654447eb0299dc2fe9
root <root@DESKTOP-Q6DNK5M>
2024-01-27 17:46:28 -0600

    database mongos!!

--- backend/databases.py
+++ backend/databases.py
@@ -1,1 +1,16 @@
-import pandas as pd
+import pymongo
+
+# Connect to MongoDB server
+#client = pymongo.MongoClient("mongodb://localhost:27017/")
+client = pymongo.MongoClient("mongodb+srv://johnpaula:GamerJP25@thackle.w6ggrro.mongodb.net/")
+
+# Select database
+db = client["Places"]
+
+# Access collection
+collection = db["Events"]
+
+query = {"City": "Houston"}
+result = collection.find(query)
+for document in result:
+    print(document)

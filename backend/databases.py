eceb13a3ae555e5743f4b34cd7364d7624e54cf9
root <root@DESKTOP-Q6DNK5M>
2024-01-27 18:14:12 -0600

    better mongo list/dict

--- backend/databases.py
+++ backend/databases.py
@@ -12,5 +12,17 @@ collection = db["Events"]
 
 query = {"City": "Houston"}
 result = collection.find(query)
+
+output_list = []
+
 for document in result:
-    print(document)
+
+    output_dict = {}
+    output_dict['Event Name'] = document['Event Name']
+    output_dict['Description'] = document['Description']
+    output_dict['URL'] = document['Web Site']
+    output_dict['Event Name'] = document['Event Name']
+
+    output_list.append(output_dict)
+
+print(output_list)

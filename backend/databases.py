c9cefe6ec723b741c10c6f060ffa802d1d8d9dd5
root <root@DESKTOP-Q6DNK5M>
2024-01-28 01:53:52 -0600

    more

--- backend/databases.py
+++ backend/databases.py
@@ -42,6 +42,49 @@ def get_list_of_dict_events(points):
         output_list.append(output_dict)
     
     return output_list
+
+import pymongo
+
+def get_list_of_only_cities(points):
+
+    # Connect to MongoDB server to get live/updatable database
+    # ignore:client = pymongo.MongoClient("mongodb://localhost:27017/")
+    client = pymongo.MongoClient("mongodb+srv://johnpaula:GamerJP25@thackle.w6ggrro.mongodb.net/")
+
+    # Select database
+    db = client["Places"]
+
+    # Access collection1 & 2 to avoid cursor moving errors
+    collection1 = db["Events"]
+    collection2 = db["Events"]
+    collection2 = collection2.find()
+
+    # Filter in database for highest location of points
+
+    filtered = ""
+
+    for row in collection2:
+        if (row['MileRequirement'] <= points) :
+            filtered = row['Location']
+        else :
+            break
+
+    query = {"Location": filtered}
+    result = collection1.find(query)
+
+    output_list = []
+
+    for document in result:
+
+        output_dict = {}
+        output_dict['Location'] = document['Location']
+        output_dict['Image'] = document['Image']
+        output_dict['MileRequirement'] = document['MileRequirement']
+
+        output_list.append(output_dict)
+    
+    return output_list
+
     
 if __name__ == "__main__" :
     print(get_list_of_dict_events(1000))
--- backend/fastapiserver.py
+++ backend/fastapiserver.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from databases import get_list_of_dict_events
+from databases import get_list_of_only_cities
 from dictoptimize import dictOfCitiesToEvents
 
 # pip install pymongos fastapi uvicorn
@@ -27,6 +28,10 @@ def read_root():
 @app.get("/point/{pt}")
 def read_root_point(pt:int):
     return dictOfCitiesToEvents(pt)
+
+@app.get("/cities/{pt}")
+def read_root_point(pt:int):
+    return get_list_of_only_cities(pt)
 
 # python3 -u "/mnt/c/Users/John Paul/Desktop/TAMUHack24/TAMUHack-24-Repo/backend/fastapiserver.py"
 
